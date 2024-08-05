dbc590a68819437510aea48b5c4316358bb6fe79
abderrahimjk <abderrahim.aitbouna@gmail.com>
2024-08-05 13:52:29 +0100

    commit

---
+++ src/main/java/ma/chat/chat_chat/ChatChatApplication.java
@@ -0,0 +1,13 @@
+package ma.chat.chat_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/chat/chat_chat/ChatChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.chat.chat_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
